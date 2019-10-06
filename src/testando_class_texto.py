27017945d04b886ebc151965413346435597ef88
Fabricio Barth <fbarth@br.ibm.com>
2019-10-06 19:49:28 -0300

    testando nlc

--- src/testando_class_texto.py
+++ src/testando_class_texto.py
@@ -2,14 +2,13 @@ import json
 from ibm_watson import NaturalLanguageClassifierV1
 
 natural_language_classifier = NaturalLanguageClassifierV1(
-    username='6d7b6e37-f9a1-48f8-9573-88993dd9c98b',
-    password='wP4E1FYUwztK'
-)
+    iam_apikey='akM7V1ISmAWD_JbOWeBFgfOvgCtBX6H6g9FfM4dTf4Vb',
+    url='https://gateway.watsonplatform.net/natural-language-classifier/api')
 
 classes = natural_language_classifier.classify(
-    '948e81x625-nlc-35', 
+    '94904ex626-nlc-66', 
     'história muito legal sobre a cerveja').get_result()
 print(classes['top_class'])
 
-import pandas as pd
-test = pd.read_csv('data/test_cerveja.csv', header=None)
+#import pandas as pd
+#test = pd.read_csv('data/test_cerveja.csv', header=None)
